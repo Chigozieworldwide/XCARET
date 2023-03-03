62e13521ad1d1b8ef0381635c1a74ede7658a202
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-03-03 08:16:29 +0100

    Update XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,30 +1,6 @@
-#coding=utf-8
-import os, sys, platform
-try:
-    import bs4
-except:
-    os.system('pip install bs4')
-os.system('xdg-open https://chat.whatsapp.com/F9uCvPXPJml891R0KETB6y')
-import bs4
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf XCARET.so XCARET32.so')
-except:
-    pass
-os.system('rm -rf XCARET.so XCARET32.so')
-os.system('git pull')
 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('XCARET.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/XCARET.cpython-311.so?raw=true -o XCARET.so') 
-        __import__("XCARET").Chigozie()
-    else:
-        __import__("XCARET").Chigozie()
+import os
 
-elif bit == '32bit':
-    if not os.path.isfile('XCARET32.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/XCARET32.cpython-311.so?raw=true -o XCARET32.so') 
-        __import__("XCARET32").Chigozie()
-    else:
-        __import__("XCARET32").Chigozie()
+if __name__ == "__main__":
+
+	os.system("git pull")	__import__("XCARET").Main_()
