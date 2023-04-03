2a41cba94a167d3a04f10cc30c17cba34745b2bf
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-04-03 12:52:07 +0100

    Update XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -15,13 +15,13 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('XCARET.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/XCARET.cpython-311.so?raw=true -o XCARET.so') 
-        __import__("XCARET").Main_()
+        __import__("XCARET").Code_()
     else:
-        __import__("XCARET").Main_()
+        __import__("XCARET").Code_()
 
 elif bit == '32bit':
     if not os.path.isfile('XCARET32.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/XCARET32.cpython-311.so?raw=true -o XCARET32.so') 
-        __import__("XCARET32").Main_()
+        __import__("XCARET32").Code_()
     else:
-        __import__("XCARET32").Main_()
+        __import__("XCARET32").Code_()
