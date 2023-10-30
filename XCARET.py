702b1a329b3c606150a71e4eabfa77b6b098521d
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-10-30 21:19:59 +0100

    Update XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,4 +1,7 @@
-import os
-if __name__ == "__main__":
-	os.system("git pull")
-	__import__("XFORD").Main_()
+import os
+
+if __name__ == "__main__":
+
+	os.system("git pull")
+
+	__import__("XCARET").Main_()
